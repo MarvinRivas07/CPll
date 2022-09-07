5aebc5f385a7c34e7a8dbec8f12d74fcb2890216
Marvin <88003076+MarvinRivas07@users.noreply.github.com>
2022-09-06 19:49:37 -0600

    C

---
+++ NotasAlumnos/NotasAlumnos/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace NotasAlumnos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtapellido = new System.Windows.Forms.TextBox();
+            this.txtasignatura = new System.Windows.Forms.TextBox();
+            this.txtLab = new System.Windows.Forms.TextBox();
+            this.txtParcial = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtpromedio = new System.Windows.Forms.TextBox();
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.rtbRegistros = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Orange;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DarkOrange;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Laboratorio";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.YellowGreen;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Asignatura";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.SkyBlue;
+            this.label6.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Parcial";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtCodigo.Enabled = false;
+            this.txtCodigo.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(85, 9);
+            this.txtCodigo.Multiline = true;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 24);
+            this.txtCodigo.TabIndex = 6;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtnombre.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombre.Location = new System.Drawing.Point(85, 40);
+            this.txtnombre.Multiline = true;
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(213, 25);
+            this.txtnombre.TabIndex = 7;
+            // 
+            // txtapellido
+            // 
+            this.txtapellido.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtapellido.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtapellido.Location = new System.Drawing.Point(85, 70);
+            this.txtapellido.Multiline = true;
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(213, 27);
+            this.txtapellido.TabIndex = 8;
+            // 
+            // txtasignatura
+            // 
+            this.txtasignatura.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtasignatura.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtasignatura.Location = new System.Drawing.Point(105, 103);
+            this.txtasignatura.Multiline = true;
+            this.txtasignatura.Name = "txtasignatura";
+            this.txtasignatura.Size = new System.Drawing.Size(163, 25);
+            this.txtasignatura.TabIndex = 9;
+            // 
+            // txtLab
+            // 
+            this.txtLab.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtLab.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLab.Location = new System.Drawing.Point(121, 135);
+            this.txtLab.Multiline = true;
+            this.txtLab.Name = "txtLab";
+            this.txtLab.Size = new System.Drawing.Size(100, 25);
+            this.txtLab.TabIndex = 10;
+            // 
+            // txtParcial
+            // 
+            this.txtParcial.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtParcial.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtParcial.Location = new System.Drawing.Point(105, 166);
+            this.txtParcial.Multiline = true;
+            this.txtParcial.Name = "txtParcial";
+            this.txtParcial.Size = new System.Drawing.Size(100, 27);
+            this.txtParcial.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.SpringGreen;
+            this.label7.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 21);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Promedio";
+            // 
+            // txtpromedio
+            // 
+            this.txtpromedio.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtpromedio.Enabled = false;
+            this.txtpromedio.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpromedio.Location = new System.Drawing.Point(105, 213);
+            this.txtpromedio.Multiline = true;
+            this.txtpromedio.Name = "txtpromedio";
+            this.txtpromedio.Size = new System.Drawing.Size(100, 27);
+            this.txtpromedio.TabIndex = 13;
+            // 
+            // btnregistrar
+            // 
+            this.btnregistrar.BackColor = System.Drawing.Color.Lime;
+            this.btnregistrar.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnregistrar.Location = new System.Drawing.Point(433, 9);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(98, 37);
+            this.btnregistrar.TabIndex = 14;
+            this.btnregistrar.Text = "Registrar";
+            this.btnregistrar.UseVisualStyleBackColor = false;
+            this.btnregistrar.Click += new System.EventHandler(this.btnregistrar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.Cyan;
+            this.btnEditar.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(592, 12);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(98, 37);
+            this.btnEditar.TabIndex = 15;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnGuardar.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(433, 91);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(98, 37);
+            this.btnGuardar.TabIndex = 16;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(592, 91);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(98, 37);
+            this.btnEliminar.TabIndex = 17;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // rtbRegistros
+            // 
+            this.rtbRegistros.BackColor = System.Drawing.Color.Gainsboro;
+            this.rtbRegistros.Font = new System.Drawing.Font("Kristen ITC", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbRegistros.Location = new System.Drawing.Point(105, 274);
+            this.rtbRegistros.Name = "rtbRegistros";
+            this.rtbRegistros.Size = new System.Drawing.Size(617, 96);
+            this.rtbRegistros.TabIndex = 18;
+            this.rtbRegistros.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Teal;
+            this.label8.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(234, 220);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 21);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Nombre";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtBuscar.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(306, 217);
+            this.txtBuscar.Multiline = true;
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(100, 27);
+            this.txtBuscar.TabIndex = 20;
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.BackColor = System.Drawing.Color.SpringGreen;
+            this.btBuscar.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBuscar.Location = new System.Drawing.Point(412, 210);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(98, 37);
+            this.btBuscar.TabIndex = 21;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(734, 410);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.rtbRegistros);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnregistrar);
+            this.Controls.Add(this.txtpromedio);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtParcial);
+            this.Controls.Add(this.txtLab);
+            this.Controls.Add(this.txtasignatura);
+            this.Controls.Add(this.txtapellido);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtapellido;
+        private System.Windows.Forms.TextBox txtasignatura;
+        private System.Windows.Forms.TextBox txtLab;
+        private System.Windows.Forms.TextBox txtParcial;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtpromedio;
+        private System.Windows.Forms.Button btnregistrar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.RichTextBox rtbRegistros;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btBuscar;
+    }
+}
+
---
+++ NotasAlumnos/NotasAlumnos/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace NotasAlumnos
+{
+    public partial class Form1 : Form
+    {
+
+        ArrayList RegistroNotas = new ArrayList();
+        int indice, numcod = 0;
+        string codigo;
+        double promedio;
+
+        void LimpiarObjetos()
+        {
+            txtnombre.Clear();
+            txtapellido.Clear();
+            txtasignatura.Clear();
+            txtLab.Clear();
+            txtParcial.Clear();
+        }
+     
+        void RegistrarNotas ()
+        {
+            codigo = " Est-00 " + numcod;
+            numcod++;
+            RegistroNotas.Add(codigo);
+            RegistroNotas.Add(txtnombre.Text);
+            RegistroNotas.Add(txtapellido.Text);
+            RegistroNotas.Add(txtasignatura.Text);
+            RegistroNotas.Add(txtLab.Text);
+            RegistroNotas.Add(txtParcial.Text);
+            promedio = ((Convert.ToDouble(txtLab.Text) + Convert.ToDouble(txtParcial.Text))/2);
+            RegistroNotas.Add(promedio);
+        }
+        void  MostrarRegistros()
+        {
+            string Regdatos = "";
+            for (int i=0; i < RegistroNotas.Count; i+=7)
+            {
+                Regdatos = Regdatos + RegistroNotas[i].ToString() + " " + RegistroNotas[i+1] + " " + RegistroNotas[i+2] + " " + RegistroNotas[i + 3] + 
+                    " " + RegistroNotas[i + 4] + " " + RegistroNotas[i + 5] + " " + RegistroNotas[i + 6] + "\n";
+            }
+            rtbRegistros.Text = Regdatos;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnregistrar_Click(object sender, EventArgs e)
+        {
+            RegistrarNotas();
+            MostrarRegistros();
+            LimpiarObjetos();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
